a52c748f31e959f1650a29f6be017352fb6084bf
Alexandros <alexandr.kyriakou@gmail.com>
2022-07-09 21:40:53 +0300

    start function

--- app.js
+++ app.js
@@ -2,16 +2,27 @@ const url = "https://course-api.com/javascript-store-products";
 
 const productsDom = document.querySelector(".products-center");
 
+// fetch function
 const fetchProducts = async () => {
   productsDom.innerHTML = '<div class="loading"></div>';
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    return data;
   } catch (error) {
     productsDom.innerHTML = `<p class="error">There was an error: ${error}</p>`;
-    console.log(error);
   }
 };
 
-fetchProducts();
+// render function
+const displayProducts = (list) => {
+  console.log(list);
+};
+
+// root function
+const start = async () => {
+  const data = await fetchProducts();
+  displayProducts(data);
+};
+
+start();
--- readme.md
+++ readme.md
@@ -1,7 +1,2 @@
-#### API Docs
-
-- (Course API)[https://course-api.com/]
-
-- (Products)[https://course-api.com/javascript-store-products]
-
-- (Single Product)[https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog]
+# Products_AJAX
+AJAX application showing products
