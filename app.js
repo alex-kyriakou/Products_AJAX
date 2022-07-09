1cd1cef3bb431661de6552981942f55a8f3fe0e8
Alexandros <alexandr.kyriakou@gmail.com>
2022-07-09 21:20:30 +0300

    loading and error functionality

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('products starter');
+const url = "https://course-api.com/javascript-store-products";
+
+const productsDom = document.querySelector(".products-center");
+
+const fetchProducts = async () => {
+  productsDom.innerHTML = '<div class="loading"></div>';
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    productsDom.innerHTML = `<p class="error">There was an error: ${error}</p>`;
+    console.log(error);
+  }
+};
+
+fetchProducts();
---
+++ readme.md
@@ -0,0 +1,7 @@
+#### API Docs
+
+- (Course API)[https://course-api.com/]
+
+- (Products)[https://course-api.com/javascript-store-products]
+
+- (Single Product)[https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog]
